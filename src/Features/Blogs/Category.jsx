ef45722302fdf236b940ed86d6569ed6d0c756d0
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-04-20 20:33:01 +0330

    feat: develop get and show categories

--- src/Features/Blogs/Category.jsx
+++ src/Features/Blogs/Category.jsx
@@ -1,30 +1,24 @@
-import Http from "@/Services/HttpService";
+import React from "react";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
 
-const Category = () => {
-  const [category, setCategory] = useState("");
-  useEffect(() => {
-    Http.get("/post-category")
-      .then(({ data }) => {
-       
-        setCategory(data.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+const Category = ({categories}) => {
+    console.log(categories)
   return (
     <div className="flex flex-col gap-5 border border-gray-100 rounded-3xl p-5 h-64 md:h-80 overflow-auto">
       <h2 className="font-extrabold text-xl">دسته بندی ها</h2>
       <ul className="child:list-disc child:text-gray-500 pr-5 space-y-5">
-        {/* {category.map(({ _id, title, englishTitle }) => {
+      <li>
+                <Link href="/blogs">همه مقاله‌ها</Link>
+              </li>
+        {categories.map(({ _id, title, englishTitle }) => {
           return (
             <React.Fragment key={_id}>
               <li>
-                <Link href={englishTitle}>{title}</Link>
+                <Link href={`/blogs/${englishTitle}`}>{title}</Link>
               </li>
             </React.Fragment>
           );
-        })} */}
+        })}
       </ul>
     </div>
   );
--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Layout from "@/Containers/Layout";
 import BlogList from "@/Features/Blogs/BlogList";
-import BlogsFeature from "@/Features/Blogs/Blogs";
 import Http from "@/Services/HttpService";
+import MobileFilterSort from "@/Features/Blogs/MobileFilterSort";
+import Sort from "@/Features/Blogs/Sort";
+import Category from "@/Features/Blogs/Category";
 
-const Blogs = ({ blogsList }) => {
+const Blogs = ({ blogsList, categories }) => {
   return (
     <Layout>
-      <BlogsFeature>
-        <BlogList blogsList={blogsList} />
-      </BlogsFeature>
+      <section className="grid md:grid-cols-12 md:grid-rows-[60px_minmax(300px,_1fr)] gap-5">
+        <div className="md:col-span-3 row-span-1">
+          <Category categories={categories}/>
+        </div>
+        <div className="md:col-span-9 md:row-span-2 row-span-2">
+          <Sort />
+          <MobileFilterSort />
+          <BlogList blogsList={blogsList} />
+        </div>
+      </section>
     </Layout>
   );
 };
@@ -17,11 +26,14 @@ const Blogs = ({ blogsList }) => {
 export default Blogs;
 
 export async function getServerSideProps(context) {
-  const { data: result } = await Http.get("/posts?page=1&limit=5");
-  const { data } = result;
+  const { data: blogsResult } = await Http.get("/posts?page=1&limit=5");
+  const { data: categoriesResult } = await Http.get("/post-category");
+  const { data: blogsData } = blogsResult;
+  const {data : categoriesData} = categoriesResult;
   return {
     props: {
-      blogsList: data,
+      blogsList: blogsData,
+      categories: categoriesData,
     },
   };
 }
